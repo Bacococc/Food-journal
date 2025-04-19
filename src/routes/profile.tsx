65f6d2e6c4a524f2123f03580ad3c65f71a1cf62
Bacococc <pphj7353@naver.com>
2025-04-19 11:36:11 +0900

    Update : function Add user profile image

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { auth, storage } from "../firebase"; 
+import styled from "styled-components";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px
+`;
+
+const AvatarUpload = styled.label`
+    width: 80px;
+    overflow: hidden;
+    height: 80px;
+    border-radius: 50%;
+    background-color: aliceblue;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    svg{
+        width: 50px;
+    }
+`;
+
+const AvatarImg = styled.img`
+    width: 100%;
+`;
+const AvatarInput = styled.input`
+    display: none;
+`;
+const Name = styled.span`
+    font-size: 30px;
+`;
+
 export default function Profile() {
-    return <h1>Profile!</h1>
+    const user = auth.currentUser;
+    const [avatar, setAvatar] = useState(user?.photoURL);
+
+    useEffect(() => {
+        if (user) {
+            setAvatar(user.photoURL);
+        }
+    }, [user]);
+
+    const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { files } = e.target;
+        if (files && files.length === 1 && user) {
+            const file = files[0];
+            const locationRef = ref(storage, `avatars/${user.uid}`);
+            const result = await uploadBytes(locationRef, file);
+            const avatarUrl = await getDownloadURL(result.ref);
+            setAvatar(avatarUrl);
+            await updateProfile(user, {
+                photoURL: avatarUrl,
+            });
+        }
+    };
+
+    return(
+    <Wrapper>
+        <AvatarUpload htmlFor="avatar">
+            {avatar ? ( 
+                <AvatarImg src={avatar} alt="avatar" /> 
+            ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+            )}
+        </AvatarUpload>
+        <AvatarInput onChange={onAvatarChange} id="avatar" type="file" accept="image/*" />
+        <Name>
+            {user?.displayName ?? "Anonymous"}
+        </Name>
+    </Wrapper>
+    )
 }
